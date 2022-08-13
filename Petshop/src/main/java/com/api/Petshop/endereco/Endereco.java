28e432cbde82ecf93a79fca2f32202ca586bd520
Lucas123-len <lucasperangel@gmail.com>
2022-08-13 17:25:15 -0300

    Criando o objeto Endereço.

---
+++ Petshop/src/main/java/com/api/Petshop/endereco/Endereco.java
@@ -0,0 +1,39 @@
+package com.api.Petshop.endereco;
+
+public class Endereco {
+	private String pais;
+	private String estado;
+	private String cidade;
+	private String bairro;
+	private String numero;
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+}
