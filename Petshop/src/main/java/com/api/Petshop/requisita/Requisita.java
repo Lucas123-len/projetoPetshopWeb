f44ca5f69a4b380585e7cf8cbbbe3ca95e3ab1dd
Lucas123-len <lucasperangel@gmail.com>
2022-08-13 23:01:57 -0300

    Criando o objeto de relacionamento Requisita.

---
+++ Petshop/src/main/java/com/api/Petshop/requisita/Requisita.java
@@ -0,0 +1,26 @@
+package com.api.Petshop.requisita;
+
+public class Requisita {
+	private int codigo;
+	private int codigoCliente;
+	private int codigoServico;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public int getCodigoCliente() {
+		return codigoCliente;
+	}
+	public void setCodigoCliente(int codigoCliente) {
+		this.codigoCliente = codigoCliente;
+	}
+	public int getCodigoServico() {
+		return codigoServico;
+	}
+	public void setCodigoServico(int codigoServico) {
+		this.codigoServico = codigoServico;
+	}
+	
+}
