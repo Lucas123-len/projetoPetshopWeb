30239741d2a9be9b7e39a1ea0226a4c75033c7c1
Lucas123-len <lucasperangel@gmail.com>
2022-08-13 22:57:17 -0300

    Alterando o objeto de relacionamento Realiza.

--- Petshop/src/main/java/com/api/Petshop/realiza/Realiza.java
+++ Petshop/src/main/java/com/api/Petshop/realiza/Realiza.java
@@ -1,38 +1,30 @@
 package com.api.Petshop.realiza;
-import com.api.Petshop.funcionario.*;
-import com.api.Petshop.servico.*;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 public class Realiza {
 	@Id
-	private String codigo;
-	private Funcionario funcionario;
-	private Servico servico;
-	public Realiza(String codigo,String codigoFuncionario,String cpf, String nome, String endereco, String codigoFuncaoFuncionario, String desc, float salario,String codigoServico,String descricao,String tipo,double valor,String dataServico) {
-		this.codigo=codigo;
-		Funcionario fc=new Funcionario(codigoFuncionario,cpf,nome,endereco,codigoFuncaoFuncionario,desc,salario);
-		Servico sr=new Servico(codigoServico,descricao,tipo,valor,dataServico);
-		this.funcionario=fc;
-		this.servico=sr;
-	}
-	public String getCodigo() {
+	private int codigo;
+	private int codigoFuncionario;
+	private int codigoServico;
+	public int getCodigo() {
 		return codigo;
 	}
-	public void setCodigo(String codigo) {
+	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
-	public Funcionario getFuncionario() {
-		return funcionario;
+	public int getCodigoFuncionario() {
+		return codigoFuncionario;
 	}
-	public void setFuncionario(Funcionario funcionario) {
-		this.funcionario = funcionario;
+	public void setCodigoFuncionario(int codigoFuncionario) {
+		this.codigoFuncionario = codigoFuncionario;
 	}
-	public Servico getServico() {
-		return servico;
+	public int getCodigoServico() {
+		return codigoServico;
 	}
-	public void setServico(Servico servico) {
-		this.servico = servico;
+	public void setCodigoServico(int codigoServico) {
+		this.codigoServico = codigoServico;
 	}
+	
 }
