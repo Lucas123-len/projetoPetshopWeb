3c968873320e27727c824fd7aa9e4520ec9b4753
Lucas123-len <lucasperangel@gmail.com>
2022-08-13 22:51:56 -0300

    Criando a objeto de relacionamento Atende.

---
+++ Petshop/src/main/java/com/api/Petshop/atende/Atende.java
@@ -0,0 +1,25 @@
+package com.api.Petshop.atende;
+
+public class Atende {
+	private int codigo;
+	private int codigoCliente;
+	private int codigoFuncionario;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public int getCodigoCliente() {
+		return codigoCliente;
+	}
+	public void setCodigoCliente(int codigoCliente) {
+		this.codigoCliente = codigoCliente;
+	}
+	public int getCodigoFuncionario() {
+		return codigoFuncionario;
+	}
+	public void setCodigoFuncionario(int codigoFuncionario) {
+		this.codigoFuncionario = codigoFuncionario;
+	}
+}
