18f81446901b384c7c754e741e3bca58e1610474
Lucas123-len <lucasperangel@gmail.com>
2022-09-04 22:42:29 -0300

    Implementando conceitos iniciais de repositorys.

---
+++ Petshop/src/main/java/com/api/Petshop/repository/ClienteRepository.java
@@ -0,0 +1,16 @@
+package com.api.Petshop.repository;
+import com.api.Petshop.cliente.*;
+import com.api.Petshop.pessoa.*;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	@Query("Select p From Pessoa p Where p.cpf = :cpf Or p.nome = :nome")
+	public List<Pessoa> findByCpfOrNome(@Param("cpf") String cpf, @Param("nome") String nome);
+}
