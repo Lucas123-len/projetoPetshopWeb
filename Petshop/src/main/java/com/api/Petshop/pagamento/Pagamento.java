b677438cf9c1e64984525b0dae310a80d3d1ac6c
Lucas123-len <lucasperangel@gmail.com>
2022-10-24 05:43:38 -0300

    fazendo alterações

---
+++ Petshop/src/main/java/com/api/Petshop/pagamento/Pagamento.java
@@ -0,0 +1,5 @@
+package com.api.Petshop.pagamento;
+
+public interface Pagamento {
+	public double getTotalPagamento();
+}
--- src/main/java/com/api/Petshop/pagamento/Pagamento.java
+++
@@ -1,5 +0,0 @@
-package com.api.Petshop.pagamento;
-
-public interface Pagamento {
-	public double getTotalPagamento();
-}
